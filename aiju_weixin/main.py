23d5c5c0d870b9f8301c34150669ed3951d7571d
EC2 Default User <ec2-user@ip-172-31-35-244.ec2.internal>
2014-12-15 21:48:40 +0000

    succeed at initializing handshake with WeChat server

--- aiju_weixin/main.py
+++ aiju_weixin/main.py
@@ -7,23 +7,27 @@ import xml.etree.ElementTree as ET
 app = Flask(__name__)
 
 APP_ROOT = '/'
-APP_TOKEN = 'aiju2014'
+APP_TOKEN = 'Aiju_NewYork_NewYork_2014'
 # verify for weixin server.
 # weixin server will send GET request first to verify this backend
 @app.route(APP_ROOT, methods=['GET'])
 def weixin_access_verify():
+    print "Handshake between WeChat's server with this Python server"
     echostr = request.args.get('echostr')
     if verification(request) and echostr is not None:
+        print " Verification successful!"
         return echostr
+	print "Verification fail :("
     return 'access verification fail'
 
 # reciever msgs from weixin server
 @app.route(APP_ROOT, methods=['POST'])
 def weixin_msg():
+    print "inside weixin_msg"
     if verification(request):
-        data = request.data
-        msg = parse_msg(data)
-        print msg
+		data = request.data
+		msg = parse_msg(data)
+		print msg
 
 def parse_msg(rawmsgstr):
     root = ET.fromstring(rawmsgstr)
@@ -33,6 +37,7 @@ def parse_msg(rawmsgstr):
     return msg
 
 def verification(req):
+    print "inside verificantion"
     signature = req.args.get('signature')
     timestamp = req.args.get('timestamp')
     nonce = req.args.get('nonce')
@@ -52,4 +57,4 @@ def verification(req):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=80)
