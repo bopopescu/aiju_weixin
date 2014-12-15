3865491bcba28c00e1710db3ab6c842b61025d3e
AWS Weixin Server <ec2-user@ip-172-31-35-244.ec2.internal>
2014-12-15 22:37:46 +0000

    achieved simple text conversation between user and backend

--- aiju_weixin/main.py
+++ aiju_weixin/main.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 
 APP_ROOT = '/'
 APP_TOKEN = 'Aiju_NewYork_NewYork_2014'
+
+RETURN_TEXT_RESPONSE = """
+                     <xml><ToUserName><![CDATA[{0}]]></ToUserName>
+                     <FromUserName><![CDATA[{1}]]></FromUserName>
+                     <CreateTime>{2}</CreateTime>
+                     <MsgType><![CDATA[text]]></MsgType>
+                     <Content><![CDATA[{3}]]></Content>
+                     </xml>
+                     """
+
 # verify for weixin server.
 # weixin server will send GET request first to verify this backend
 @app.route(APP_ROOT, methods=['GET'])
@@ -15,7 +25,7 @@ def weixin_access_verify():
     print "Handshake between WeChat's server with this Python server"
     echostr = request.args.get('echostr')
     if verification(request) and echostr is not None:
-        print " Verification successful!"
+        print " Verification success!"
         return echostr
 	print "Verification fail :("
     return 'access verification fail'
@@ -25,9 +35,26 @@ def weixin_access_verify():
 def weixin_msg():
     print "inside weixin_msg"
     if verification(request):
-		data = request.data
-		msg = parse_msg(data)
-		print msg
+        data = request.data
+        msg = parse_msg(data)
+        #print data
+        #print
+        #print type(msg)
+        usr_msg =  msg["Content"]
+        usr_open_id = msg["FromUserName"]
+        app_id = msg["ToUserName"]
+
+        #print usr_msg
+        #print usr_open_id
+        #print app_id
+
+    return return_text_msg_to_wechat(app_id, usr_open_id, usr_msg)
+
+def return_text_msg_to_wechat(app_id, usr_open_id, usr_msg):
+    print "return text msg to user"
+    resp_create_time = int(time.time())
+    resp_msg = "I am AIJU. You just sent: {0} to me.".format(usr_msg)
+    return RETURN_TEXT_RESPONSE.format(usr_open_id,app_id,resp_create_time,resp_msg)
 
 def parse_msg(rawmsgstr):
     root = ET.fromstring(rawmsgstr)
@@ -54,7 +81,6 @@ def verification(req):
     if hashstr == signature:
         return True
     return False
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
