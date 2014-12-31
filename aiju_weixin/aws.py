30ead108934f22481abe4678a85a901076e25c13
greenland <ec2-user@ip-172-31-35-244.ec2.internal>
2014-12-31 22:51:15 +0000

    aws.py retrieve img

--- aiju_weixin/aws.py
+++ aiju_weixin/aws.py
@@ -2,18 +2,34 @@
 
 from boto.s3.connection import S3Connection
 import ConfigParser
+import requests
 
 config = ConfigParser.ConfigParser()
 config.read('/home/ec2-user/aiju_weixin/config.cfg')
 
 AWS_WX_BUCKET=config.get('aj_aws','aj_wx_bucket')
 
-def upload_to_s3(objects)
-	
-	if objects is None:
-		return -1
+s3_conn = None
 
-	conn = S3Connection()
-	bucket = conn.get_bucket(AWS_WX_BUCKET)
+def upload_img_to_s3(img_url)
+	global s3_conn	
+
+	if img_url is None or img_url == "":
+		return -1
+	
+	if s3_conn is None:
+		s3_conn = S3Connection()
+	
+	# retrieve img
+	response = requests.get(img_url, stream=True)
+	
+	if not response.ok:
+		print "error"
+	
+	for block in response.iter_content(1024):
+		if not block:
+			break
+
+	bucket = s3_conn.get_bucket(AWS_WX_BUCKET)
 	
 	
